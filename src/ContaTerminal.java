0d70ac5a27d27c375051c534bc035e0c05d48510
clebson2039 <clebsonfarias1@gmail.com>
2025-01-14 16:31:53 -0300

    Att

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -2,22 +2,57 @@ import java.util.Scanner;
 
 public class ContaTerminal {
 
-    
-    public static void main(String[] args) {
-        
-       
-       Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
 
-       System.out.println("Nome completo: ");
-       String nome = scan.nextLine();
-       System.out.println("Agencia: ");
-       String agencia = scan.nextLine();
-       System.out.println("Número da conta: ");
-       int numeroConta = scan.nextInt();
-       System.out.println("Adicione seu saldo: ");
-       double saldovalor = scan.nextDouble();
-       
-       System.out.println(":----------------------------------------------------------------------:");
-       System.out.println("Olá " + nome + "\nObrigado por criar uma conta em nosso banco, sua agencia é: " + agencia + "\nSua conta: " + numeroConta + "\ne seu saldo é de: " + saldovalor + ", já está disponível para saque.");
+    /*public void solicitarNome() {
+        System.out.println("Nome completo: ");
+        String nome = scan.nextLine();
+        System.out.println("Olá " + nome);
+    }
+
+    public void solicitarAgencia() {
+        System.out.println("Agencia: ");
+        String agencia = scan.nextLine();
+        System.out.println("obrigado por criar uma conta em nosso banco, sua agência é: " + agencia);
+    }
+
+    public void solicitarConta() {
+        System.out.println("Número da conta: ");
+        String conta = scan.nextLine();
+        System.out.println("conta: " + conta);
+    }
+
+    public void solicitarSaldo() {
+        System.out.println("Adicione seu saldo: ");
+        double saldoConta = scan.nextDouble();
+        System.out.println("e seu saldo: " + saldoConta + "já está disponível para saque");
+    }*/
+
+    public void solicitandoDados() {
+
+        System.out.println("Nome completo: ");
+        String nome = scan.nextLine();
+
+        System.out.println("Agencia: ");
+        String agencia = scan.nextLine();
+
+        System.out.println("Número da conta: ");
+        int conta = scan.nextInt();
+
+        System.out.println("Adicione seu saldo: ");
+        double saldoConta = scan.nextDouble();
+
+
+        if (nome == ""){
+            System.out.println("Digite seu nome");
+        }else if(agencia == ""){
+            System.out.println("Digite sua agência");
+        }else if(conta < 0) {
+            System.out.println("Digite sua conta");
+        }else if(saldoConta < 0.0){
+            System.out.println("Digite seu saldo");
+        }
+        System.out.println(":---------------------------------------------------:");
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco. \nSua agência é: " + agencia + "\nConta: " + conta + "\nE seu saldo: " + saldoConta + ", já está disponível para saque.");
     }
 }
---
+++ src/Usuario.java
@@ -0,0 +1,14 @@
+public class Usuario {
+    
+    public static void main(String[] args) {
+        ContaTerminal contaTerminal = new ContaTerminal();
+
+        /*contaTerminal.solicitarNome();
+        contaTerminal.solicitarAgencia();
+        contaTerminal.solicitarConta();
+        contaTerminal.solicitarSaldo();*/
+
+        contaTerminal.solicitandoDados();
+
+    }
+}
